523cfe6d9117b4865c975f52029e1a489f190444
enlore <n.e.lorenson@gmail.com>
2014-01-24 14:07:42 -0600

    New question

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,35 @@
  * GET home page.
  */
 
+var Question = require('../models/question')
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    Question.find(function (err, questions) {
+        if (err) throw err 
+        res.render('index', { title: 'Test Maker', questions: questions });
+    })
 };
+
+exports.new_question = function (req, res) {
+    if ('GET' === req.method) {
+        res.render('new_question') 
+    }
+
+    if ('POST' === req.method) {
+        console.log(req.body) 
+        question = new Question({
+            question: req.body.question,
+            answers: [
+                req.body.answer_1, 
+                req.body.answer_2, 
+                req.body.answer_3, 
+                req.body.answer_4, 
+            ]
+        })
+
+        question.save(function (err) {
+            if (err) throw err 
+            res.redirect('/')
+        })
+    }
+}
